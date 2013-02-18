9c3a9a49dfa17e1f03da3c7b54cedf6502aa0ae3
Alexey <alexej.bazhenov@gmail.com>
2013-02-18 20:06:32 +0100

    path change

--- javascripts/s3mp.js
+++ javascripts/s3mp.js
@@ -177,7 +177,7 @@ S3MP.prototype.signPartRequests = function(id, object_name, upload_id, parts, cb
     return memo + "-" + part.size;
   }, parts[0].size);
 
-  url = "s3_multipart/uploads/"+id;
+  url = "/s3_multipart/uploads/"+id;
   body = JSON.stringify({ object_name     : object_name,
                           upload_id       : upload_id,
                           content_lengths : content_lengths
@@ -190,7 +190,7 @@ S3MP.prototype.signPartRequests = function(id, object_name, upload_id, parts, cb
 S3MP.prototype.completeMultipart = function(uploadObj, cb) {
   var url, body, xhr;
 
-  url = 's3_multipart/uploads/'+uploadObj.id;
+  url = '/s3_multipart/uploads/'+uploadObj.id;
   body = JSON.stringify({ object_name    : uploadObj.object_name,
                           upload_id      : uploadObj.upload_id,
                           content_length : uploadObj.size,
